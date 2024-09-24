934ab94174494d93f751c3333e7cf1023503bcf2
Nikolaj Rask <83177244+NikolajRask@users.noreply.github.com>
2024-09-24 20:11:55 +0200

    Fixed 404

--- src/docs/utils/use-404.ts
+++ src/docs/utils/use-404.ts
@@ -4,16 +4,17 @@ import useSettings from "./settings/use-settings"
 
 export default function use404() {
 
-    const { allTitles, page, setPage } = useDocsContext()
+    const { allTitles, page, setPage, isLoading } = useDocsContext()
 
     useEffect(() => {
-
+      if (allTitles().length > 0) {
         if (!allTitles().includes(page)) {
-            if (page?.trim() == "" || page?.trim() == null || page?.trim() == undefined || page == useSettings().defaultPage) {
-              setPage(useSettings().defaultPage)
-            } else {
-              setPage(useSettings().missingPage)
-            }
+          if (page?.trim() == "" || page?.trim() == null || page?.trim() == undefined || page == useSettings().defaultPage) {
+            setPage(useSettings().defaultPage)
+          } else {
+            setPage(useSettings().missingPage)
+          }
         }
-    }, [page])
+      }
+    }, [page, isLoading, allTitles()])
 }
