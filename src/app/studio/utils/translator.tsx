f54467e397305d7f0fe158cbdb2c970d0b0b3c2a
Nikolaj Rask <83177244+NikolajRask@users.noreply.github.com>
2024-10-16 21:02:41 +0200

    feat(studio): Only adds the components that it uses in the code

--- src/app/studio/utils/translator.tsx
+++ src/app/studio/utils/translator.tsx
@@ -107,17 +107,20 @@ const markdownToJsx = (text: string): (string | JSX.Element)[] => {
 };
 
 
-const markdownToCode = (text: string): string[] => {
+const markdownToCode = (text: string): [string[], string[]] => {
   // Split text into parts based on different Markdown elements and newline (\n)
   const parts = text.split(/(\*\*\*.*?\*\*\*|\*\*.*?\*\*|\*.*?\*|`[^`]+`|\[.*?\]\(.*?\)|!\[.*?\]\(.*?\)|^# .*$|^## .*$|^> .*|\/.*?\/|==.*?==|\n)/gm);
 
+  const usedComponents:string[] = []
+
   // Map over the parts and transform them into JSX elements where applicable
-  return parts.map((part) => {
+  const code = parts.map((part) => {
     // Match newline (\n) and render <br />
     if (part === '\n') {
       return "\n      <br/>";
     }
     if (part.match(/`([^`]+)`/)) {
+      usedComponents.push("Code")
       return `\n      <Code code={${"`"}${part.slice(1, -1)}${"`"}}
       />`;
     }
@@ -125,6 +128,7 @@ const markdownToCode = (text: string): string[] => {
     else if (part.match(/!\[(.*?)\]\((.*?)\)/)) {
       const match = part.match(/!\[(.*?)\]\((.*?)\)/);
       if (match) {
+        usedComponents.push("Image")
         return `\n      <Image src="${match[2]}" alt="${match[1]}" />`;
       }
     }
@@ -132,6 +136,7 @@ const markdownToCode = (text: string): string[] => {
     else if (part.match(/\[(.*?)\]\((.*?)\)/)) {
       const match = part.match(/\[(.*?)\]\((.*?)\)/);
       if (match) {
+        usedComponents.push("Link")
         return (
           `\n      <Link href="${match[2]}">
             ${match[1]}
@@ -141,18 +146,22 @@ const markdownToCode = (text: string): string[] => {
     }
     // Match blockquotes (> text)
     else if (part.match(/^> (.*)$/m)) {
+      usedComponents.push("Blockquote")
       return `\n      <Blockquote>${part.slice(2).trim()}</Blockquote>`;
     }
     // Match headers (## Heading 2)
     else if (part.match(/^## (.*)$/)) {
+      usedComponents.push("Title")
       return `\n      <Title>${part.slice(3).trim()}</Title>`;
     }
     // Match headers (# Heading 1)
     else if (part.match(/^# (.*)$/)) {
+      usedComponents.push("Header")
       return `\n      <Header>${part.slice(2).trim()}</Header>`;
     }
     // Match custom /text/ -> <p>text</p>
     else if (part.match(/\/(.*?)\//)) {
+      usedComponents.push("CommandPrompt")
       return `\n      <CommandPrompt content={${"`"}${part.slice(1, -1)}${"`"}}/>`;
     }
     // Match bold and italic (***text***)
@@ -169,22 +178,28 @@ const markdownToCode = (text: string): string[] => {
     }
     // Match custom ==text== (highlighted text)
     else if (part.match(/==(.*?)==/)) {
+      usedComponents.push("Highlight")
       return `\n      <Highlight>{"${part.slice(2, -2)}"}</Highlight>`;
     }
     // Return plain text if no match
+    usedComponents.push("Text")
     return part ? `\n      <Text>${part}</Text>` : "";
   }).filter(Boolean); // Filter out any null values
+
+  const usedComponentsUnique = Array.from(new Set(usedComponents))
+
+  return [code, usedComponentsUnique]
 };
     
 export function TranslateCode(content: string,  pageName: string): string {
 
   let code2 = ``
 
-  const code = markdownToCode(content).map((x) => {return `${x}`})
+  const [code, usedComponents] = markdownToCode(content)
 
   let lastItem: string | undefined = undefined
 
-  code.forEach((x) => {
+  code.map((x) => {return `${x}`}).forEach((x) => {
 
     let skip = false
 
@@ -206,7 +221,7 @@ export function TranslateCode(content: string,  pageName: string): string {
   return `/* eslint-disable react/no-unescaped-entities */
 
 import React from 'react'
-import { CommandPrompt, Link, SEO, Text, Card, Code, CodePreview, Header, Highlight, Image, Title, Blockquote } from '../ui/components/core'
+import { ${usedComponents.join(", ")} } from '../ui/components/core'
 
 const ${pageName?.replaceAll(" ","")} = () => {
   return (
